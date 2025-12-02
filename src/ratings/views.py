9a9cbe16ad2163a8fbe8dbc668a777789659053d
HARSH PATHANIA <hp3031@HARSHs-MacBook-Pro.local>
2025-12-01 23:27:22 -0500

    updated views.py

--- src/ratings/views.py
+++ src/ratings/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.contrib.contenttypes.models import ContentType
+from django.views.decorators.http import require_http_methods
+
+from ml import tasks as ml_tasks
 
 from .models import Rating
 # Create your views here.
@@ -19,9 +22,24 @@ def rate_movie_view(request):
         ctype = ContentType.objects.get(app_label='movies' , model='movie')
         rating_obj = Rating.objects.create(content_type=ctype ,  object_id = object_id, value =rating_value, user = user)
         if rating_obj.content_object is not None:
-            message = "<span class='bg-success text-light py-1 px-3 rounded'>Rating Saved.</div>"
-            response = HttpResponse("Skipping", status=200)
-            response['HX-Trigger-After-Settle'] = 'did-rate-movie'
-            return response
+           total_new_suggestions = request.session.get("total-new-suggestions") or 0
+           items_rated = request.session.get('items-rated') or 0
+           items_rated +=1
+           request.session['items-rated'] = items_rated
+           print('items_rated' , items_rated)
+           if items_rated % 5 == 0 :
+               print("trigger new suggestions")
+               users_ids = [user.id]
+               ml_tasks.batch_users_prediction_task.apply_async(
+                   kwargs ={
+                   "users_ids" : users_ids,
+                   "start_page" :total_new_suggestions,
+                   "max_pages" : 10
+                   }
+               )
+           message = "<span class='bg-success text-light py-1 px-3 rounded'>Rating Saved.</div>"
+           response = HttpResponse(message, status=200)
+           response['HX-Trigger-After-Settle'] = 'did-rate-movie'
+           return response
     return HttpResponse(message, status=200)
 
